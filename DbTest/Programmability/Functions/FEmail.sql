2f6ac56e864845ee9ae5c5c7a5fbfe84944bcde9
Nico7522 <nico.daddabbo7100@gmail.com>
2024-01-12 10:45:40 +0100

    ajout fonction pour récupérer l'id et envoyer dans mail (test)

---
+++ DbTest/Programmability/Functions/FEmail.sql
@@ -0,0 +1,12 @@
+﻿CREATE FUNCTION [dbo].[FEmail]
+(
+	@Email NVARCHAR(100)
+	
+)
+RETURNS INT
+AS
+BEGIN
+	DECLARE @UserId INT
+	SET @UserId = (SELECT Id FROM Utilisateur WHERE Email = @Email)
+	RETURN @UserId
+END
--- DbTest/Programmability/SP/SPUtilisateurRegister.sql
+++ DbTest/Programmability/SP/SPUtilisateurRegister.sql
@@ -8,16 +8,16 @@ AS
 
 BEGIN
 	
-	DECLARE @PasswordHash BINARY(64), @SecurityStamp UNIQUEIDENTIFIER
-
+	DECLARE @PasswordHash BINARY(64), @SecurityStamp UNIQUEIDENTIFIER, @UserId INT, @BodyMessage NVARCHAR(150)
 	SET @SecurityStamp = NEWID()
 	SET @PasswordHash = dbo.FHasher(TRIM(@Password), @SecurityStamp)
-
 	INSERT INTO [Utilisateur] (Nom, Prenom, Email, DateNaissance, PasswordHash, SecurityStamp)
 	VALUES (TRIM(@Nom), TRIM(@Prenom), TRIM(@Email), @DateNaissance, @PasswordHash, @SecurityStamp)
+	SET @UserId = dbo.FEmail(@Email)
+	SET @BodyMessage = 'Your id is ' + CAST(@UserId AS NVARCHAR(MAX))
 	EXEC msdb.dbo.sp_send_dbmail   
 	@profile_name = 'Nicolas',  
 	@recipients = @Email,  
-	@body = 'The stored procedure finished successfully.', 
+	@body = @BodyMessage,
 	@subject = 'Automated Success Message';
 END
